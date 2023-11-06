52a640d5588b4d1a7c26e6ce4ad9f529d7c6fd80
basu1020 <bvp1020mu@gmail.com>
2023-11-06 06:51:22 +0530

    completed code for getting vehicles api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VehiclesModule } from './vehicles/vehicles.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [VehiclesModule],
+  imports: [
+    VehiclesModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST.toString(),
+      port: 3306,
+      username: process.env.DB_USER.toString(),
+      password: process.env.DB_PASSWORD.toString(),
+      database: process.env.DB_DATABASE.toString(),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/vehicles/vehicles.controller.ts
+++ src/vehicles/vehicles.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { VehiclesService } from './vehicles.service';
 
 @Controller('vehicles')
-export class VehiclesController {}
+export class VehiclesController {
+    constructor(private readonly vehiclesService: VehiclesService) {}
+
+    @Get()
+    getVehicles(){
+        return this.vehiclesService.getVehicles()
+    }
+
+    @Get("/wheels-options")
+    getDifferentWheelsOptions(){
+        return this.vehiclesService.getDifferentWheelsOptions()
+    }
+
+    @Get("/wheels/:wheels")
+    getVehicleTypesBasedOnNoOfVehicles(@Param('wheels', ParseIntPipe) wheels: 4 | 2) {
+        return this.vehiclesService.getVehicleTypesBasedOnNoOfWheels(wheels)
+    }
+
+    @Get("/type/:type")
+    getModelsBasedOnType(@Param('type') type: 'hatchback' | 'suv' | 'sedan' | 'cruiser' | 'sports'){
+        return this.vehiclesService.getModelsBasedOnType(type)
+    }
+
+}
